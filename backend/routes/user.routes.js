ec2c4e8d4d6dcd70dbd49fbf4ea6e3693e3b5b20
Khodor Hassoun <khodorhassoun@outlook.com>
2022-10-15 23:41:45 +0300

    Backend - users.controller: get all users

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,10 @@
 const authMiddleware = require('../middlewares/auth.middleware')
+const {getAllUsers} = require('../controllers/users.controller')
 const {Router} = require('express');
 const router = Router();
 
 // router.get('/', getAllUsers);
-router.get('/', authMiddleware);
+router.get('/', authMiddleware, getAllUsers);
 // router.get('/:id', getUser);
 // router.post('/', createUser);
 // router.put('/', updateUser);
