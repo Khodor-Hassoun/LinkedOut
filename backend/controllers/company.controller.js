b735217b8019857dfb427e77696164e3bd1c3a4d
Khodor Hassoun <khodorhassoun@outlook.com>
2022-10-16 12:20:36 +0300

    Backend - company.controller: update company details

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -14,4 +14,29 @@ const getCompany = async (req, res) =>{
 
 }
 
+const updateCompany = async (req, res) =>{
+    const {id} = req.body
+    const company = await Company.findById(id)
+    let {name, email, password, bio, profile_picture, banner} = req.body
+    let emaill
+    if(email){
+        if(await Company.findOne({email:email})){
+            res.status(400).json({message:"Error"})
+            return
+        }
+    }else{emaill = company.email}
+
+    company.name = name ? name : company.name
+    company.email = emaill
+    company.password = password ? await bcrypt.hash(password, 10) : company.password
+    company.bio = bio ? bio : company.bio
+    company.profile_picture = profile_picture ?  `${saveImage(profile_picture).localPath}${saveImage(profile_picture).filename}` : company.profile_picture
+    company.banner = banner ? `${saveImage(banner).localPath}${saveImage(banner).filename}` : company.banner
+
+    await company.save()
+    res.json(company)
+
+
+}
+
 module.exports = {getAllCompanies, getCompany}
